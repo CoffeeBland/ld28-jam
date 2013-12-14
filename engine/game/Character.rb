808891e2c4a4aa9116250287ad33d508414908ea
Dagothig <Dagothig@github.com>
2013-12-14 16:41:52 -0500

    Actions

--- engine/game/Character.rb
+++ engine/game/Character.rb
@@ -1,4 +1,20 @@
 require "engine/game/entity"
 
 class Character < Entity
+  :attr_reader current_action
+  def do action
+    @current_action = action
+  end
+
+  def update delta, world
+    if self.current_action != nil
+      self.current_action.update delta, world, self
+
+      if self.current_action.completed?
+        @current_action = nil
+      end
+    end
+
+    super delta, world
+  end
 end
