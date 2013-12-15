e265540ca7aa346fccb31f8b595f1ad6f5f07290
Frederic Gingras <frederic@gingras.cc>
2013-12-14 20:05:14 -0500

    Menu text

--- ld28/states/logo.rb
+++ ld28/states/logo.rb
@@ -18,7 +18,6 @@ module LD28
         set_color 0xFFFFFFFF
         draw_rect 0, 0, @game.width, @game.height
         Images[:logo].draw_centered
-        #Text.draw ["TROLOLOL", "It ain't"], 80, 80, 0xff000000
       end
 
       def init
--- ld28/states/menu.rb
+++ ld28/states/menu.rb
@@ -10,12 +10,19 @@ module LD28
 
       def draw
         transition 0x000000FF, 0, 1000, @now_in_for
+        set_color 0xFFFFFFFF
+        draw_rect 0, 0, @game.width, @game.height
+        Text.draw ["Le One Kingdom"], 50, 50, 0xff000000
+        Text.draw ["Press the enter key to play"], 50, @game.height - 50, 0xff000000
       end
 
       def init
         super
         self.input_press Gosu::KbEscape, Proc.new {
           @game.close
+        }
+        self.input_press Gosu::KbEnter, Proc.new {
+
         }
       end
 
